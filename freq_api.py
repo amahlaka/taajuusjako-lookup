bf59b58dad220b76488a5bb1100e87ed6ce67069
Arttu Mahlakaarto <arttu.mahlakaarto@gmail.com>
2022-04-12 01:16:46 +0300

    Added index page

--- freq_api.py
+++ freq_api.py
@@ -29,6 +29,7 @@ def createaccount():
 
 
 
+
 @app.route('/api/freq/<freq>')
 def get_allocations_for_frequency(freq):
     """
@@ -65,7 +66,7 @@ def get_allocations_for_frequency_html(freq):
 
 
 
-@app.route(('/list-allocations/<freq>/<bandwidth>'), methods=['GET'])
+@app.route(('/frequency/<freq>/<bandwidth>'), methods=['GET'])
 def list_allocations(freq,bandwidth):
     """
     Render a HTML table of allocations for a specific frequency
@@ -96,6 +97,32 @@ def filter_uniques(field: str, value: str):
     """
     allocations = search_by_field_value(field, value.lower(), find_unique_allocations())
     return render_template('frequencies.html', title="Unique Allocations with Filters", allocations=allocations)
+
+@app.route('/')
+def index():
+    """
+    Index page
+    """
+    return render_template_string("""
+    <html>
+    <head>
+    <title>Frequency API</title>
+    </head>
+    <body>
+    <h1>Frequency API</h1>
+    <p>This is a simple API that returns the allocation of a frequency</p>
+    <p>Endpoint: /api/freq/freq  where freq is the frequency in Hz, kHz, MHz or GHz</p>
+    <p>Example: /api/freq/433MHz</p>
+    <p>Endpoint: /frequency/freq  where freq is the frequency in Hz, kHz, MHz or GHz, Returns results in HTML Table</p>
+    <p>Example: /frequency/433MHz</p>
+    <p>Endpoint: /frequency/freq/width Returns matches with freq+-width</p>
+    <p>Example: /frequency/2.4GHz/1GHz</p>
+    <p>Example: /unique-allocations</p>
+    <p>Example: /unique-allocations/field/value value can be a comma seperated list of values to filter with</p>
+    <p>Example: /unique-allocations/Sub_band_usage/amateur,amateur-satellite returns results where Sub-band Usage has any of the defined values</p>
+    </body>
+    </html>
+    """)
 
 def main():
     """
